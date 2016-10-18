59c06f9500dc795a6a6f57e93500b09324b4513d
yuichi123 <k013c1368@it-neec.jp>
2016-10-19 03:42:45 +0900

    排他処理追加 master用パラメータ変更

--- app/libraries/InfraredTransmitter.php
+++ app/libraries/InfraredTransmitter.php
@@ -2,22 +2,22 @@
 
 class InfraredTransmitter {
   public static function send($value) {
-    // ---排他処理開始---
-    // $sem_id = sem_get(ftok(__FILE__, 'g'), 1);
-    // if (!sem_acquire($sem_id)) {
-    //     throw new Exception('sem_acquire failed');
-    // }
+    //---排他処理開始---
+    $sem_id = sem_get(ftok(__FILE__, 'g'), 1);
+    if (!sem_acquire($sem_id)) {
+        throw new Exception('sem_acquire failed');
+    }
 
     // ---赤外線送信処理---
     $is_test_mode = (ENVIRONMENT == 'development' ? 1 : 0);
-    $exe_command = 'C:\bin\sendir ' . $value . ' ' . $is_test_mode;
+    $exe_command = 'sendir ' . $value . ' ' . $is_test_mode;
     $output = array();
     exec($exe_command, $output);
 
-    // ---排他処理終了---
-    // if (!sem_release($res)) {
-    //     throw new Exception('sem_release failed');
-    // }
+    //---排他処理終了---
+    if (!sem_release($res)) {
+        throw new Exception('sem_release failed');
+    }
     
     return $output;
   }
